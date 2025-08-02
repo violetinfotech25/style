3bafa6c63b4e3994ddc9ba6bac19d1bd8141cc9e
violetinfotech25 <violetinfotech25@gmail.com>
2025-08-02 14:58:18 +0530

    Resolved conflict in manage.py

--- manage.py
+++ manage.py
@@ -3,14 +3,9 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magudaedu_project.settings')
-=======
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magudam.settings')
->>>>>>> merge-fix
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,7 +15,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
